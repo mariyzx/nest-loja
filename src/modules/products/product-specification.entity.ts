37b2b73a5f8b76fb470b7db118d07df0721cdeb6
Mariana Werneck <marinhomariana8@gmail.com>
2025-10-18 00:14:08 -0300

    feat: implement product module with CRUD operations; add DTOs, entities, and repository methods

---
+++ src/modules/products/product-specification.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { ProductEntity } from './product.entity';
+
+@Entity({ name: 'product_specifications' })
+export class ProductSpecificationsEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ name: 'name', type: 'varchar', length: 200, nullable: false })
+  name: string;
+  @Column({
+    name: 'description',
+    type: 'varchar',
+    length: 1000,
+    nullable: false,
+  })
+  description: string;
+
+  @ManyToOne(() => ProductEntity, (produto) => produto.specifications, {
+    orphanedRowAction: 'delete',
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  product: ProductEntity;
+}
