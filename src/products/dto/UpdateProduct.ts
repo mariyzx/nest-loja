686fe81845c3cd71ae2355513b4339a48fcb4b69
Mariana Werneck <marinhomariana8@gmail.com>
2025-10-17 21:56:28 -0300

    feat: refactor UpdateProductDTO to extend CreateProductDTO and simplify validation rules

--- src/products/dto/UpdateProduct.ts
+++ src/products/dto/UpdateProduct.ts
@@ -1,48 +1,30 @@
+import { IsOptional } from 'class-validator';
 import {
-  ArrayMinSize,
-  IsArray,
-  IsNotEmpty,
-  IsOptional,
-  IsPositive,
-  MinLength,
-} from 'class-validator';
-import { ProductImageDTO, ProductSpecificationDTO } from './CreateProduct.dto';
+  CreateProductDTO,
+  ProductImageDTO,
+  ProductSpecificationDTO,
+} from './CreateProduct.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateProductDTO {
-  @IsNotEmpty({ message: 'Name cannot be empty' })
+export class UpdateProductDTO extends PartialType(CreateProductDTO) {
   @IsOptional()
   name?: string;
 
-  @IsPositive({ message: 'Value must be a positive number' })
   @IsOptional()
   value?: number;
 
-  @IsPositive({ message: 'Value must be a positive number' })
   @IsOptional()
   availableQuantity?: number;
 
-  @IsNotEmpty({ message: 'Description cannot be empty' })
-  @MinLength(1000, {
-    message: 'Description must have at least 1000 characters',
-  })
   @IsOptional()
   description?: string;
 
-  @IsArray({ message: 'Specifications must be an array' })
-  @ArrayMinSize(3, {
-    message: 'Specifications must have at least 3 items',
-  })
   @IsOptional()
   specifications?: ProductSpecificationDTO[];
 
-  @IsArray({ message: 'Images must be an array' })
-  @ArrayMinSize(1, {
-    message: 'Images must have at least 1 item',
-  })
   @IsOptional()
   images?: ProductImageDTO[];
 
-  @IsNotEmpty({ message: 'Category cannot be empty' })
   @IsOptional()
   category?: string;
 
