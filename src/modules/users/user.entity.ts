2c15f4226053e03df7be40b25059ea30dc7671a8
Mariana Werneck <marinhomariana8@gmail.com>
2025-10-18 00:17:18 -0300

    feat: implement user module with CRUD operations; add DTOs, entity, repository, and service for user management

---
+++ src/modules/users/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  OneToMany,
+} from 'typeorm';
+import { OrderEntity } from '../order/order.entity';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name', type: 'varchar', length: 200, nullable: false })
+  name: string;
+
+  @Column({ name: 'email', type: 'varchar', length: 200, nullable: false })
+  email: string;
+
+  @Column({ name: 'password', type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+
+  @OneToMany(() => OrderEntity, (order) => order.user)
+  orders: OrderEntity[];
+}
