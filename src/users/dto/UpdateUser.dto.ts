ce444ac769c23c71f8b7dce591ac7a50118161e8
Mariana Werneck <marinhomariana8@gmail.com>
2025-10-17 21:54:01 -0300

    feat: refactor UpdateUserDTO to extend CreateUserDTO and simplify validation

--- src/users/dto/UpdateUser.dto.ts
+++ src/users/dto/UpdateUser.dto.ts
@@ -1,17 +1,14 @@
-import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
-import { isUniqueEmail } from '../validation/unique-email.validator';
+import { IsOptional } from 'class-validator';
+import { CreateUserDTO } from './CreateUser.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateUserDTO {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+export class UpdateUserDTO extends PartialType(CreateUserDTO) {
   @IsOptional()
   name: string;
 
-  @IsEmail(undefined, { message: 'O email não é válido' })
-  @isUniqueEmail({ message: 'Já existe um usuário com esse email' })
   @IsOptional()
   email: string;
 
-  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
   @IsOptional()
   password: string;
 }
