59fcc5378b082f7ebf150a76a74b2cf8fa45d7af
Mariana Werneck <marinhomariana8@gmail.com>
2025-10-21 18:38:46 -0300

    feat: update JwtModule configuration to use async registration with ConfigService

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,16 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../users/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
     UserModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '72h' },
+        };
+      },
+      inject: [ConfigService],
       global: true,
-      secret: 'SEGREDO_SECRETO',
-      signOptions: { expiresIn: '72h' },
     }),
   ],
 })
